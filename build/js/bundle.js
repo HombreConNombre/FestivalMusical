3ea61520df48ea6a8f15671097585ad6ce07c193
Astrock <masterlord2100@gmail.com>
2021-09-15 11:13:32 +0200

    0.9v gallery finished
    Ampliaciones y cierres realizadas.

---
+++ build/js/bundle.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria();
+});
+
+function crearGaleria() {
+    const galeria = document.querySelector('.galeria-imagenes');
+    for (let i = 1; i <= 12; i++) {
+        const imagen = document.createElement('IMG');
+        imagen.src = 'build/src/img/thumb/' + i + '.webp';
+        const lista = document.createElement('LI');
+        lista.appendChild(imagen);
+        galeria.appendChild(lista);
+
+        imagen.classList.add('fotoConciertos');
+        imagen.onclick = ampliarImagen;
+        imagen.dataset.imagenID = i
+    }
+}
+
+function ampliarImagen(e) {
+    const id = parseInt(e.target.dataset.imagenID);
+    const imagen = document.createElement('IMG');
+    imagen.src = 'build/src/img/grande/' + id + '.webp';
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    //Boton para cerrar la imagen grande
+    const cerrarImagen = document.createElement('P');
+    cerrarImagen.textContent = 'X';
+    cerrarImagen.classList.add('botonCerrar');
+    overlay.appendChild(cerrarImagen);
+
+    //Mostrar en el HTML
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fijar-body');
+
+
+    cerrarImagen.onclick =
+        function() {
+            console.log("HOLIWI");
+            overlay.remove();
+            body.classList.remove('fijar-body');
+        }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,54 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+//const imagein = require('gulp-imagein');
+const webp = require('gulp-webp');
+const concat = require('gulp-concat');
 
-function compilarCSS(fin) {
-    return src("./scss/app.scss")
-        .pipe(sass())
-        .pipe(dest("./build/css"));
-    fin();
+const paths = {
+    imagenes: 'src/img/**/*',
+    scss: 'scss/**/*.scss',
+    js: 'JS/**/*.js'
 }
 
-function observadorArchivos(fin) {
-    watch("scss/*.scss", compilarCSS);
-    fin()
+function compilarCSS() {
+    return src(paths.scss)
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(dest("./build/css"));
+}
+// Función para GULP que autocomprime las imágenes. No funciona por directrices ES.
+//function imagenes() {
+//    return src('src/img/**/*')
+//        .pipe(imagein())
+//        .pipe(dest('./build/img'));
+//}
+
+function observadorArchivos() {
+    watch(paths.scss, compilarCSS)
+    watch(paths.js, javaScript)
+}
+
+function javaScript() {
+    return src(paths.js)
+        .pipe(concat('bundle.js'))
+        .pipe(dest('./build/js'))
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest('./build/img'))
 }
 
 exports.compilarCSS = compilarCSS;
+/**
+ * exports.imagenes = imagenes;
+ */
+exports.versionWebp = versionWebp;
 exports.observadorArchivos = observadorArchivos;
+exports.javaScript = javaScript;
+exports.default = series(compilarCSS, javaScript, versionWebp, observadorArchivos);
+
 // Series determina el orden que se va a llevar a cabo. Parallel las ejecuta todas a la vez(hace falta importarlo de Gulp)
 //exports.cola = series(compilarCSS);
